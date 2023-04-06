9515f99339615e164d389fba4b4c2749385b4d98
AbhijeetVashishtha <abhijeetvashishth13@gmail.com>
2023-04-06 23:23:38 +0530

    Made Chat App Dynamic

--- public/client.js
+++ public/client.js
@@ -1,1 +1,39 @@
 const socket = io();
+
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message__area');
+let name;
+
+do{
+    name = prompt('Enter your name: ');
+}while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter')
+    {
+        sendMessage(e.target.value);
+    }
+})
+
+function sendMessage(message){
+    let msg = {
+        user: name,
+        message: message
+    }
+
+    appendMessage(msg, 'outgoing');
+}
+
+function appendMessage(msg, type){
+    let mainDiv = document.createElement('div');
+    let className = type;
+    mainDiv.classList.add(className, 'message');
+
+    let markup = `
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+
+    mainDiv.innerHTML = markup;
+    messageArea.appendChild(mainDiv);
+}
